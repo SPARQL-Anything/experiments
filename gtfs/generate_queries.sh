0f902ef4a9c01e5147dd5973623fae03b911e926
Luigi Asprino <luigi.asprino@gmail.com>
2022-04-07 17:26:14 +0200

    xml query templates

--- gtfs/generate_queries.sh
+++ gtfs/generate_queries.sh
@@ -14,6 +14,7 @@ do
     #filename="$(basename -- $QUERY_FILE)"
     #filename="q$query.sparql"
     QUERY_TEMPLATE_FILE=$SA_QUERIES/"q$query.sparql"
+    QUERY_TEMPLATE_FILE_XML=$SA_QUERIES/"q${query}_xml.sparql"
     echo "Processing $QUERY_TEMPLATE_FILE"
 
     # CSV
@@ -27,6 +28,13 @@ do
     sed -e "s/%size/$size/g" -e "s/%strategy/1/g" -e "s/%slice/false/g" -e "s/%format/json/g" -e "s/%param/blank-nodes=true/g"  $QUERY_TEMPLATE_FILE > $size/"q$query-json-strategy1-no_slice.sparql"
     sed -e "s/%size/$size/g" -e "s/%strategy/0/g" -e "s/%slice/true/g" -e "s/%format/json/g" -e "s/%param/blank-nodes=true/g"  $QUERY_TEMPLATE_FILE > $size/"q$query-json-strategy0-slice.sparql"
     sed -e "s/%size/$size/g" -e "s/%strategy/1/g" -e "s/%slice/true/g" -e "s/%format/json/g" -e "s/%param/blank-nodes=true/g"  $QUERY_TEMPLATE_FILE > $size/"q$query-json-strategy1-slice.sparql"
+
+
+    # XML
+    sed -e "s/%size/$size/g" -e "s/%strategy/0/g" -e "s/%slice/false/g" -e "s/%format/xml/g" -e "s/%param/blank-nodes=true/g" $QUERY_TEMPLATE_FILE_XML > $size/"q$query-xml-strategy0-no_slice.sparql"
+    sed -e "s/%size/$size/g" -e "s/%strategy/1/g" -e "s/%slice/false/g" -e "s/%format/xml/g" -e "s/%param/blank-nodes=true/g"  $QUERY_TEMPLATE_FILE_XML > $size/"q$query-xml-strategy1-no_slice.sparql"
+    sed -e "s/%size/$size/g" -e "s/%strategy/0/g" -e "s/%slice/true/g" -e "s/%format/xml/g" -e "s/%param/blank-nodes=true/g"  $QUERY_TEMPLATE_FILE_XML > $size/"q$query-xml-strategy0-slice.sparql"
+    sed -e "s/%size/$size/g" -e "s/%strategy/1/g" -e "s/%slice/true/g" -e "s/%format/xml/g" -e "s/%param/blank-nodes=true/g"  $QUERY_TEMPLATE_FILE_XML > $size/"q$query-xml-strategy1-slice.sparql"
 
 
   done
