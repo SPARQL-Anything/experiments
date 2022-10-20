80e10091341b1d46acb7b8cf2cc332a6c16ab00e
Luigi Asprino <luigi.asprino@gmail.com>
2022-10-20 10:34:55 +0200

    add ondisk field to the analysis

--- gtfs/result_analyser.py
+++ gtfs/result_analyser.py
@@ -1,6 +1,7 @@
-import pandas as pd
-import numpy as np
 import os
+
+import numpy as np
+import pandas as pd
 
 
 def get_status_string(status_list):
@@ -33,169 +34,83 @@ def get_status_list(status_list):
 
 
 input_sizes = [1, 10, 100, 1000]
+
+configurations = [
+    {"slice": "no_slice", "strategy": "strategy0", "ondisk": False},
+    {"slice": "no_slice", "strategy": "strategy1", "ondisk": False},
+    {"slice": "slice", "strategy": "strategy1", "ondisk": False},
+    {"slice": "no_slice", "strategy": "strategy1", "ondisk": True}
+]
 strategies = ["strategy0", "strategy1"]
 slice = ["no_slice", "slice"]
-memory_limits = [256, 512, 1024, 4096]
+memory_limits = [256, 512, 1024, 4096, 8192, 16384, 32768]
 format = "csv"
-experiment_folder = "/Users/lgu/workspace/spice/CogComplexityAndPerformaceEvaluation/gtfs/"
-measures_folder = f"{experiment_folder}measures_{format}/"
-out_folder = f"summary_{format}"
-#out_folder = "aggregated_measures_xml_1_10_100_1000"
+experiment_folder = "/Users/lgu/workspace/SPARQLAnything/CogComplexityAndPerformaceEvaluation/gtfs/202210015-experiment_csv_1_10_100_1000/"
+out_folder = f"{experiment_folder}summary_{format}"
 
 if not os.path.exists(out_folder):
     os.mkdir(out_folder)
 
+records_summary_time = [
+    [f"Format", "Query", "Strategy", "Slice", "Size", "Ondisk", "Memory", "Mean", "Std", "Status-1", "Status-2",
+     "Status-3"]]
+
 for input_size in input_sizes:
 
-    records_time = []
-    records_mem = []
+    records_time_per_size = []
 
     for q in range(1, 19):
 
-        time_df = pd.read_csv(f"{measures_folder}time_q{q}_{format}.tsv", sep='\t')
-        mem_df = pd.read_csv(f"{measures_folder}mem_q{q}_{format}.tsv", sep='\t')
+        time_df = pd.read_csv(f"{experiment_folder}time_q{q}_{format}.tsv", sep='\t')
 
-        records_time.append([f"Q{q}", "M256-MEAN", "M256-STD", "M256-Status",
-                        "M512-MEAN", "M512-STD", "M512-Status",
-                        "M1024-MEAN", "M1024-STD", "M1024-Status",
-                        "M4096-MEAN", "M4096-STD", "M4096-Status"])
+        time_headers = [f"Q{q}"]
+        for memory_limit in memory_limits:
+            time_headers.append(f"M{memory_limit}-MEAN")
+            time_headers.append(f"M{memory_limit}-STD")
+            time_headers.append(f"M{memory_limit}-Status")
+        records_time_per_size.append(time_headers)
 
-        records_mem.append([f"Q{q}", "M256-CPU-MEAN", "M256-CPU-STD", "M256-RSS-MEAN", "M256-RSS-STD", "M256-Status",
-                         "M512-CPU-MEAN", "M512-CPU-STD", "M512-RSS-MEAN", "M512-RSS-STD", "M512-Status",
-                         "M1024-CPU-MEAN", "M1024-CPU-STD", "M1024-RSS-MEAN", "M1024-RSS-STD", "M1024-Status",
-                         "M4096-CPU-MEAN", "M4096-CPU-STD", "M4096-RSS-MEAN", "M4096-RSS-STD", "M4096-Status"])
+        for configuration in configurations:
+            s = configuration["slice"]
+            strategy = configuration["strategy"]
+            ondisk = configuration["ondisk"]
 
-        for s in slice:
-            for strategy in strategies:
-                record = [f"{strategy}-{s}"]
-                record_mem = [f"{strategy}-{s}"]
+            record_per_size = [f"{strategy}-{s}-ondisk:{ondisk}"]
 
-                for memory_limit in memory_limits:
-                    r = time_df[(time_df["InputSize"] == input_size) &
+            for memory_limit in memory_limits:
+                r = time_df[(time_df["InputSize"] == input_size) &
                             (time_df["Strategy"] == strategy) &
                             (time_df["Slice"] == s) &
                             (time_df["MemoryLimit"] == memory_limit) &
+                            (time_df["Ondisk"] == ondisk) &
                             (time_df["Run"] != "AVERAGE")
-                           ]
+                            ]
 
-                    mem_runs = mem_df[(mem_df["InputSize"] == input_size) &
-                                (mem_df["Strategy"] == strategy) &
-                                (mem_df["Slice"] == s) &
-                                (mem_df["MemoryLimit"] == memory_limit)
-                                ]
+                record_summary_time = [format, f"q{q}", strategy, s, input_size, ondisk, memory_limit]
 
-                    times = np.array(r["Time"])
+                times = np.array(r["Time"])
 
-                    # cpus_1 = np.array(mem_runs[mem_runs["Run"] == "RUN1"]["%cpu"])
-                    # cpus_2 = np.array(mem_runs[mem_runs["Run"] == "RUN2"]["%cpu"])
-                    # cpus_3 = np.array(mem_runs[mem_runs["Run"] == "RUN3"]["%cpu"])
-                    cpu = np.array(mem_runs["%cpu"])
+                if len(times) > 0:
+                    record_per_size.append(f"{times.mean():.1f}".replace(".", ","))
+                    record_per_size.append(f"{times.std():.1f}".replace(".", ","))
 
-                    # rss_1 = np.array(mem_runs[mem_runs["Run"] == "RUN1"]["%rss"])
-                    # rss_2 = np.array(mem_runs[mem_runs["Run"] == "RUN2"]["%rss"])
-                    # rss_3 = np.array(mem_runs[mem_runs["Run"] == "RUN3"]["%rss"])
-                    rss = np.array(mem_runs["rss"])
+                    record_summary_time.append(f"{times.mean():.1f}".replace(".", ","))
+                    record_summary_time.append(f"{times.std():.1f}".replace(".", ","))
+                else:
+                    record_per_size.append("NA")
+                    record_per_size.append("NA")
 
-                    if len(times) > 0:
-                        record.append(f"{times.mean():.1f}".replace(".", ","))
-                        record.append(f"{times.std():.1f}".replace(".", ","))
-                    else:
-                        record.append("NA")
-                        record.append("NA")
+                    record_summary_time.append("NA")
+                    record_summary_time.append("NA")
 
-                    if len(cpu) > 0:
-                        record_mem.append(f"{cpu.mean():.1f}".replace(".", ","))
-                        record_mem.append(f"{cpu.std():.1f}".replace(".", ","))
-                    else:
-                        record_mem.append("NA")
-                        record_mem.append("NA")
+                record_per_size.append(get_status_string(list(r[r["STDErr"].notna()]["STDErr"])))
 
-                    if len(rss) > 0:
-                        record_mem.append(f"{rss.mean():.1f}".replace(".", ","))
-                        record_mem.append(f"{rss.std():.1f}".replace(".", ","))
-                    else:
-                        record_mem.append("NA")
-                        record_mem.append("NA")
+                record_summary_time.extend(get_status_list(list(r[r["STDErr"].notna()]["STDErr"])))
+                records_summary_time.append(record_summary_time)
 
-                    record.append(get_status_string(list(r[r["STDErr"].notna()]["STDErr"])))
-                    record_mem.append(get_status_string(list(r[r["STDErr"].notna()]["STDErr"])))
+            records_time_per_size.append(record_per_size)
 
-                records_time.append(record)
-                records_mem.append(record_mem)
+    pd.DataFrame(records_time_per_size).to_csv(f"{out_folder}/aggr_time_{input_size}_{format}.tsv", sep='\t', index=False,
+                                               header=False)
 
-
-    pd.DataFrame(records_time).to_csv(f"{out_folder}/aggr_time_{input_size}_{format}.tsv", sep='\t', index=False, header=False)
-    pd.DataFrame(records_mem).to_csv(f"{out_folder}/aggr_mem_{input_size}_{format}.tsv", sep='\t', index=False, header=False)
-
-
-## generate summary
-records_time = []
-records_time_with_mem = []
-
-records_time.append([f"Format", "Query", "Strategy", "Slice", "Size", "Memory", "Mean", "Std",
-                         "Status-1", "Status-2", "Status-3"])
-
-records_time_with_mem.append([f"Format", "Query", "Strategy", "Slice", "Size", "Memory", "Mean", "Std",
-                         "Status-1", "Status-2", "Status-3", "CPU-MEAN", "CPU-STD", "RSS-MEAN", "RSS-STD"])
-
-
-for input_size in input_sizes:
-    for q in range(1, 19):
-        time_df = pd.read_csv(f"{measures_folder}time_q{q}_{format}.tsv", sep='\t')
-        mem_df = pd.read_csv(f"{measures_folder}mem_q{q}_{format}.tsv", sep='\t')
-        for s in slice:
-            for strategy in strategies:
-                for memory_limit in memory_limits:
-                    record = [format, f"q{q}", strategy, s, input_size, memory_limit]
-
-                    r = time_df[(time_df["InputSize"] == input_size) &
-                            (time_df["Strategy"] == strategy) &
-                            (time_df["Slice"] == s) &
-                            (time_df["MemoryLimit"] == memory_limit) &
-                            (time_df["Run"] != "AVERAGE")
-                           ]
-
-                    times = np.array(r["Time"])
-
-                    if len(times) > 0:
-                        record.append(f"{times.mean():.1f}".replace(".", ","))
-                        record.append(f"{times.std():.1f}".replace(".", ","))
-                    else:
-                        record.append("NA")
-                        record.append("NA")
-
-                    record.extend(get_status_list(list(r[r["STDErr"].notna()]["STDErr"])))
-                    records_time.append(record)
-
-                    record_with_mem = [r for r in record]
-
-                    mem_runs = mem_df[(mem_df["InputSize"] == input_size) &
-                                      (mem_df["Strategy"] == strategy) &
-                                      (mem_df["Slice"] == s) &
-                                      (mem_df["MemoryLimit"] == memory_limit)
-                                      ]
-
-                    cpu = np.array(mem_runs["%cpu"])
-                    rss = np.array(mem_runs["rss"])
-
-                    if len(cpu) > 0:
-                        record_with_mem.append(f"{cpu.mean():.1f}".replace(".", ","))
-                        record_with_mem.append(f"{cpu.std():.1f}".replace(".", ","))
-                    else:
-                        record_with_mem.append("NA")
-                        record_with_mem.append("NA")
-
-                    if len(rss) > 0:
-                        record_with_mem.append(f"{rss.mean():.1f}".replace(".", ","))
-                        record_with_mem.append(f"{rss.std():.1f}".replace(".", ","))
-                    else:
-                        record_with_mem.append("NA")
-                        record_with_mem.append("NA")
-
-                    records_time_with_mem.append(record_with_mem)
-
-
-pd.DataFrame(records_time).to_csv(f"{out_folder}/summary_{format}.tsv", sep='\t', index=False, header=False)
-pd.DataFrame(records_time_with_mem).to_csv(f"{out_folder}/summary_with_mem_{format}.tsv", sep='\t', index=False, header=False)
-
-
+pd.DataFrame(records_summary_time).to_csv(f"{out_folder}/summary_{format}.tsv", sep='\t', index=False, header=False)
